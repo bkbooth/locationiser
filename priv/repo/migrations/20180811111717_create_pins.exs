37a030e75c2b8f0bba6e2a46c96dc746810c0adb
Ben Booth <ben.booth@lendi.com.au>
2019-05-31 17:03:30 +1000

    Fix all formatter changes

--- priv/repo/migrations/20180811111717_create_pins.exs
+++ priv/repo/migrations/20180811111717_create_pins.exs
@@ -3,12 +3,12 @@ defmodule Locationiser.Repo.Migrations.CreatePins do
 
   def change do
     create table(:pins, primary_key: false) do
-      add :id, :binary_id, primary_key: true
-      add :lat, :decimal
-      add :lng, :decimal
-      add :title, :string
-      add :description, :text
-      add :user_id, references(:users, on_delete: :delete_all, type: :binary_id), null: false
+      add(:id, :binary_id, primary_key: true)
+      add(:lat, :decimal)
+      add(:lng, :decimal)
+      add(:title, :string)
+      add(:description, :text)
+      add(:user_id, references(:users, on_delete: :delete_all, type: :binary_id), null: false)
 
       timestamps()
     end
