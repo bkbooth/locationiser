0d44e67239833de76b9bedbacec8ae15c09680a5
Ben Booth <bkbooth@gmail.com>
2019-06-04 23:21:54 +1000

    Use styled-components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
+import { createGlobalStyle } from 'styled-components/macro';
 import logo from './logo.svg';
-import './App.css';
+import { Header, Link, Logo, Wrapper } from './App.styles';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Wrapper>
+      <GlobalStyle />
+      <Header>
+        <Logo src={logo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <Link href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
-        </a>
-      </header>
-    </div>
+        </Link>
+      </Header>
+    </Wrapper>
   );
 }
 
