69e549c7027b7fe46b15c4a843dc741243932887
Ben Booth <bkbooth@gmail.com>
2019-06-12 00:38:31 +1000

    Update tests and snapshots

--- src/components/Emoji.test.js
+++ src/components/Emoji.test.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Emoji, { emojis } from './Emoji';
 
+it('renders a pin emoji', () => {
+  const { container } = render(<Emoji emoji={emojis.pin} />);
+  expect(container.cloneNode(true)).toMatchSnapshot();
+});
+
 it('renders a waiting emoji', () => {
   const { container } = render(<Emoji emoji={emojis.waiting} />);
   expect(container.cloneNode(true)).toMatchSnapshot();
