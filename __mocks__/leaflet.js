5ff0f7b26ec95433bcc2ac0a78c8ea607a5981cd
Ben Booth <bkbooth@gmail.com>
2019-06-12 16:01:11 +1000

    Update leaflet mocks

--- __mocks__/leaflet.js
+++ __mocks__/leaflet.js
@@ -1,8 +1,42 @@
 const L = {
   map: jest.fn(() => ({
     setView: jest.fn(),
+    addControl: jest.fn(),
+    removeControl: jest.fn(),
+    zoomControl: null,
+    dragging: {
+      enable: jest.fn(),
+      disable: jest.fn(),
+    },
+    touchZoom: {
+      enable: jest.fn(),
+      disable: jest.fn(),
+    },
+    doubleClickZoom: {
+      enable: jest.fn(),
+      disable: jest.fn(),
+    },
+    scrollWheelZoom: {
+      enable: jest.fn(),
+      disable: jest.fn(),
+    },
+    boxZoom: {
+      enable: jest.fn(),
+      disable: jest.fn(),
+    },
+    keyboard: {
+      enable: jest.fn(),
+      disable: jest.fn(),
+    },
+    tap: {
+      enable: jest.fn(),
+      disable: jest.fn(),
+    },
   })),
   tileLayer: jest.fn(),
+  control: {
+    zoom: jest.fn(() => Object.create()),
+  },
 };
 
 export default L;
