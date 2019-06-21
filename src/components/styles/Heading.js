39595b8b1dd8cd93236b72d39c0ee8da0faebca6
Ben Booth <bkbooth@gmail.com>
2019-06-21 23:39:33 +1000

    Fix theme shade colour scale

--- src/components/styles/Heading.js
+++ src/components/styles/Heading.js
@@ -4,7 +4,7 @@ export const Heading = styled.h1`
   font-size: 1.5rem;
   letter-spacing: 0.1rem;
   text-transform: uppercase;
-  color: ${({ theme }) => theme.colours.shade['600']};
+  color: ${({ theme }) => theme.colours.shade['400']};
   margin-top: ${({ theme }) => theme.sizes.nil};
   margin-bottom: ${({ theme }) => theme.sizes.lg};
 `;
