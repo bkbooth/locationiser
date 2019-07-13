56e32205ff9ea80c592bf9c1ec38685fdfcc7ff7
Ben Booth <bkbooth@gmail.com>
2019-07-13 11:31:59 +1000

    Add zoom to database seeded pins

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -23,6 +23,7 @@ alias Locationiser.{Accounts, Locations}
   Locations.create_pin(mario, %{
     lat: "-37.8253632",
     lng: "144.9503893",
+    zoom: 16,
     title: "Southbank, Melbourne",
     description:
       "Southbank draws a sophisticated crowd to its cultural venues, which include the Malthouse theater, based in a restored brewery, and the state-of-the-art Melbourne Recital Centre which hosts classical concerts."
@@ -32,6 +33,7 @@ alias Locationiser.{Accounts, Locations}
   Locations.create_pin(mario, %{
     lat: "-37.8309747",
     lng: "144.9610451",
+    zoom: 16,
     title: "Melbourne Zoo",
     description:
       "Modelled on London Zoo, this family-friendly establishment has animals from all around the world."
@@ -41,6 +43,7 @@ alias Locationiser.{Accounts, Locations}
   Locations.create_pin(mario, %{
     lat: "-37.8179694",
     lng: "144.9178756",
+    zoom: 16,
     title: "Royal Botanic Gardens Melbourne",
     description:
       "36-hectare botanic garden founded in 1846, planted with Australian species and non-native gardens."
@@ -57,6 +60,7 @@ alias Locationiser.{Accounts, Locations}
   Locations.create_pin(luigi, %{
     lat: "-27.4467653",
     lng: "153.0090062",
+    zoom: 16,
     title: "Suncorp Stadium",
     description:
       "52,500-capacity stadium hosting rugby and soccer home games for Brisbane teams, plus major concerts."
@@ -66,6 +70,7 @@ alias Locationiser.{Accounts, Locations}
   Locations.create_pin(luigi, %{
     lat: "-27.4692931",
     lng: "153.0564987",
+    zoom: 16,
     title: "Eat Street Northshore",
     description:
       "Lively venue at a former container wharf featuring stalls with international street eats & beer."
@@ -75,6 +80,7 @@ alias Locationiser.{Accounts, Locations}
   Locations.create_pin(luigi, %{
     lat: "-27.4592813",
     lng: "153.0304701",
+    zoom: 16,
     title: "Fortitude Valley, Brisbane",
     description:
       "Fortitude Valley is packed with nightlife, offering everything from dive bars and mellow live music venues to rooftop cocktail lounges and pumping dance clubs."
