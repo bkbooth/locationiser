b884a6e477fbb0cd8122e45be9dfbe2d857405f6
Ben Booth <bkbooth@gmail.com>
2019-07-22 11:10:17 +1000

    Add API deploy script to Travis CI
    Only test/check API

--- api/scripts/deploy.sh
+++ api/scripts/deploy.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash
+set -ev
 
 git remote add gigalixir https://$GIGALIXIR_EMAIL:$GIGALIXIR_API_KEY@git.gigalixir.com/$GIGALIXIR_APP_NAME.git
-git push -fv gigalixir HEAD:refs/heads/master
+git push -fv gigalixir `git subtree split --prefix=api master`:master
